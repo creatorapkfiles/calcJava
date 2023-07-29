bf29a18b1acd8cafd68ddf8405167d9781b1b0af
creatorapkfiles <67171093+creatorapkfiles@users.noreply.github.com>
2023-07-29 20:01:09 +0300

    Add files via upload

--- calcJava/src/Main.java
+++ calcJava/src/Main.java
@@ -2,142 +2,155 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
+
+    public static String[] calc(String stringInput) {
+        String[] inputData = stringInput.split(" ");
+        if (inputData.length != 3) {
+            Scanner newlyEnteredString = new Scanner(System.in);
+            System.out.println("Неверный формат ввода данных, попробуйте еще раз(убедитесь что введенное вами арифметическое действие соответствует формату(опреанд_пробел_оператор_пробел_операнд))");
+            stringInput = newlyEnteredString.nextLine();
+            return calc(stringInput);
+        } else {
+            return inputData;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
 
         Scanner scanner = new Scanner(System.in);
-        String str = scanner.nextLine();
-        String strArray[] = str.split(" ");
+        String stringInput = scanner.nextLine();
+        String[] stringsArray = calc(stringInput);
 
         int out;
 
         try {
-            if (strArray.length == 3) {
+            if (stringsArray.length == 3) {
 
-                if (contains(strArray[0]) == true && contains(strArray[2]) == true) {
-                    switch (strArray[1]) {
-                        case "+":
-                            out = Integer.parseInt(pre(strArray[0])) + Integer.parseInt(pre(strArray[2]));
-                            reshenIe(out);
-                            break;
-                        case "-":
-                            out = Integer.parseInt(pre(strArray[0])) - Integer.parseInt(pre(strArray[2]));
+                if (checkingForAMatch(stringsArray[0]) && checkingForAMatch(stringsArray[2])) {
+                    switch (stringsArray[1]) {
+                        case "+" -> {
+                            out = Integer.parseInt(enumSolutionWithRoman(stringsArray[0])) + Integer.parseInt(enumSolutionWithRoman(stringsArray[2]));
+                            outputWithRoman(out);
+                        }
+                        case "-" -> {
+                            out = Integer.parseInt(enumSolutionWithRoman(stringsArray[0])) - Integer.parseInt(enumSolutionWithRoman(stringsArray[2]));
                             if (out <= 0) {
                                 throw new IOException("в римской системе нет отрицательных чисел");
                             }
-                            reshenIe(out);
-                            break;
-                        case "*":
-                            out = Integer.parseInt(pre(strArray[0])) * Integer.parseInt(pre(strArray[2]));
-                            reshenIe(out);
-                            break;
-                        case "/":
-                            out = Integer.parseInt(pre(strArray[0])) / Integer.parseInt(pre(strArray[2]));
-                            reshenIe(out);
-                            break;
-                        default:
-                            throw new IOException("формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+                            outputWithRoman(out);
+                        }
+                        case "*" -> {
+                            out = Integer.parseInt(enumSolutionWithRoman(stringsArray[0])) * Integer.parseInt(enumSolutionWithRoman(stringsArray[2]));
+                            outputWithRoman(out);
+                        }
+                        case "/" -> {
+                            out = Integer.parseInt(enumSolutionWithRoman(stringsArray[0])) / Integer.parseInt(enumSolutionWithRoman(stringsArray[2]));
+                            outputWithRoman(out);
+                        }
+                        default ->
+                                throw new IOException("формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
                     }
                 } else {
-                    if (Integer.parseInt(strArray[0]) < 11 && Integer.parseInt(strArray[2]) < 11) {
-                        switch (strArray[1]) {
-                            case "+":
-                                System.out.println(Integer.parseInt(strArray[0]) + Integer.parseInt(strArray[2]));
-                                break;
-                            case "-":
-                                System.out.println(Integer.parseInt(strArray[0]) - Integer.parseInt(strArray[2]));
-                                break;
-                            case "*":
-                                System.out.println(Integer.parseInt(strArray[0]) * Integer.parseInt(strArray[2]));
-                                break;
-                            case "/":
-                                System.out.println(Integer.parseInt(strArray[0]) / Integer.parseInt(strArray[2]));
-                                break;
-                            default:
-                                throw new IOException("формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+                    if (Integer.parseInt(stringsArray[0]) < 11 && Integer.parseInt(stringsArray[2]) < 11) {
+                        switch (stringsArray[1]) {
+                            case "+" ->
+                                    System.out.println(Integer.parseInt(stringsArray[0]) + Integer.parseInt(stringsArray[2]));
+                            case "-" ->
+                                    System.out.println(Integer.parseInt(stringsArray[0]) - Integer.parseInt(stringsArray[2]));
+                            case "*" ->
+                                    System.out.println(Integer.parseInt(stringsArray[0]) * Integer.parseInt(stringsArray[2]));
+                            case "/" ->
+                                    System.out.println(Integer.parseInt(stringsArray[0]) / Integer.parseInt(stringsArray[2]));
+                            default ->
+                                    throw new IOException("формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
                         }
                     } else System.out.println("Введенное число больше десяти!!!");
                 }
             } else System.out.println("Введенное выражение неверно!");
         } catch (NumberFormatException exception){
-            if (contains(strArray[0]) == true && contains(strArray[2]) == false || contains(strArray[0]) == false && contains(strArray[2]) == true)
+            if (checkingForAMatch(stringsArray[0]) && !checkingForAMatch(stringsArray[2]) || !checkingForAMatch(stringsArray[0]) && checkingForAMatch(stringsArray[2]))
                 throw new IOException("используются одновременно разные системы счисления");
             else
                 throw new IOException("строка не является математической операцией");
         }
     }
-    public static String pre(String test) {
-        for (Rim c : Rim.values()) {
-            if (c.name().equals(test)) {
-                test = Integer.toString(c.getInteger());
-                return test;
+    static String enumSolutionWithRoman(String theStringBeingChecked) {
+        for (Rim string : Rim.values()) {
+            if (string.name().equals(theStringBeingChecked)) {
+                theStringBeingChecked = Integer.toString(string.getInteger());
+                return theStringBeingChecked;
             }
         }
-        return test;
+        return theStringBeingChecked;
     }
-    public static boolean contains(String test) {
-        for (Rim c : Rim.values()) {
-            if (c.name().equals(test)) {
-                return true;
-            }
-        }
+    static boolean checkingForAMatch(String theStringBeingChecked) {
+        for (Rim string : Rim.values()) if (string.name().equals(theStringBeingChecked)) return true;
         return false;
     }
-    public static String pre1(String test) {
-        for (Rim c : Rim.values()) {
-            if (c.getInteger() == Integer.parseInt(test)) {
-                test = c.toString();
-                return test;
+    static String solutionWithRoman(String theStringBeingChecked) {
+        for (Rim string : Rim.values()) {
+            if (string.getInteger() == Integer.parseInt(theStringBeingChecked)) {
+                theStringBeingChecked = string.toString();
+                return theStringBeingChecked;
             }
         }
-        return test;
+        return theStringBeingChecked;
     }
-    public static void reshenIe (int out){
-        if (out >= 10)
+    static void outputWithRoman (int output){
+        if (output >= 10)
         {
             int a, b;
-            a = out / 10;
-            b = out % 10;
-            switch (a){
-                case 1 :
+            a = output / 10;
+            b = output % 10;
+            switch (a) {
+                case 1 -> {
                     if (b == 0) System.out.print("X");
-                    else System.out.print("X"+pre1(Integer.toString(b)));
-                    break;
-                case 2 :
+                    else System.out.print("X" + solutionWithRoman(Integer.toString(b)));
+                }
+                case 2 -> {
                     if (b == 0) System.out.print("XX");
-                    else System.out.print("XX"+pre1(Integer.toString(b)));
-                    break;
-                case 3 :
+                    else System.out.print("XX" + solutionWithRoman(Integer.toString(b)));
+                }
+                case 3 -> {
                     if (b == 0) System.out.print("XXX");
-                    else System.out.print("XXX"+pre1(Integer.toString(b)));
-                    break;
-                case 4 :
+                    else System.out.print("XXX" + solutionWithRoman(Integer.toString(b)));
+                }
+                case 4 -> {
                     if (b == 0) System.out.print("XL");
-                    else System.out.print("XL"+pre1(Integer.toString(b)));
-                    break;
-                case 5 :
+                    else System.out.print("XL" + solutionWithRoman(Integer.toString(b)));
+                }
+                case 5 -> {
                     if (b == 0) System.out.print("L");
-                    else System.out.print("L"+pre1(Integer.toString(b)));
-                    break;
-                case 6 :
+                    else System.out.print("L" + solutionWithRoman(Integer.toString(b)));
+                }
+                case 6 -> {
                     if (b == 0) System.out.print("LX");
-                    else System.out.print("LX"+pre1(Integer.toString(b)));
-                    break;
-                case 7:
+                    else System.out.print("LX" + solutionWithRoman(Integer.toString(b)));
+                }
+                case 7 -> {
                     if (b == 0) System.out.print("LXX");
-                    else System.out.print("LXX"+pre1(Integer.toString(b)));
-                    break;
-                case 8:
+                    else System.out.print("LXX" + solutionWithRoman(Integer.toString(b)));
+                }
+                case 8 -> {
                     if (b == 0) System.out.print("LXXX");
-                    else System.out.print("LXXX"+pre1(Integer.toString(b)));
-                    break;
-                case 9:
+                    else System.out.print("LXXX" + solutionWithRoman(Integer.toString(b)));
+                }
+                case 9 -> {
                     if (b == 0) System.out.print("XC");
-                    else System.out.print("XC"+pre1(Integer.toString(b)));
-                    break;
-                case 10:
+                    else System.out.print("XC" + solutionWithRoman(Integer.toString(b)));
+                }
+                case 10 -> {
                     if (b == 0) System.out.print("C");
-                    else System.out.print("C"+pre1(Integer.toString(b)));
-                    break;
+                    else System.out.print("C" + solutionWithRoman(Integer.toString(b)));
+                }
+            }
+        } else {
+            String outputResult;
+            for (Rim string : Rim.values()) {
+                if (string.getInteger() == output) {
+                    outputResult = string.toString();
+                    System.out.println(outputResult);
+                }
             }
         }
     }
